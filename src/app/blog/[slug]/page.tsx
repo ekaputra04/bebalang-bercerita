cbba483ad9a4cff8f1626db798363b4a70bed1b1
ekaputra04 <ekaputrajuniawan@gmail.com>
2025-06-22 13:02:44 +0800

    chore: init project
    feat: add UI components including Badge, Button, Card, Separator, and Tooltip
    
    - Implemented Badge component with variants for different styles.
    - Created Button component with support for different sizes and variants.
    - Developed Card component with subcomponents for structured content display.
    - Added Separator component for visual separation of content.
    - Introduced Tooltip component for displaying additional information on hover.
    
    feat: add blog data handling and markdown processing
    
    - Implemented functions to read and process MDX files for blog posts.
    - Added markdownToHTML function to convert markdown content to HTML.
    - Created getPost and getBlogPosts functions for fetching blog data.
    
    feat: add resume data structure
    
    - Created a comprehensive resume data structure including personal information, work experience, education, projects, and hackathons.
    
    feat: utility functions for class names and date formatting
    
    - Added utility function `cn` for merging class names.
    - Implemented `formatDate` function for user-friendly date formatting.
    
    chore: configure Tailwind CSS and TypeScript
    
    - Set up Tailwind CSS configuration with custom themes and animations.
    - Configured TypeScript with strict settings and module resolution.

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,106 @@
+import { getBlogPosts, getPost } from "@/data/blog";
+import { DATA } from "@/data/resume";
+import { formatDate } from "@/lib/utils";
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
+
+export async function generateStaticParams() {
+  const posts = await getBlogPosts();
+  return posts.map((post) => ({ slug: post.slug }));
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: {
+    slug: string;
+  };
+}): Promise<Metadata | undefined> {
+  let post = await getPost(params.slug);
+
+  let {
+    title,
+    publishedAt: publishedTime,
+    summary: description,
+    image,
+  } = post.metadata;
+  let ogImage = image ? `${DATA.url}${image}` : `${DATA.url}/og?title=${title}`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "article",
+      publishedTime,
+      url: `${DATA.url}/blog/${post.slug}`,
+      images: [
+        {
+          url: ogImage,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [ogImage],
+    },
+  };
+}
+
+export default async function Blog({
+  params,
+}: {
+  params: {
+    slug: string;
+  };
+}) {
+  let post = await getPost(params.slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <section id="blog">
+      <script
+        type="application/ld+json"
+        suppressHydrationWarning
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "BlogPosting",
+            headline: post.metadata.title,
+            datePublished: post.metadata.publishedAt,
+            dateModified: post.metadata.publishedAt,
+            description: post.metadata.summary,
+            image: post.metadata.image
+              ? `${DATA.url}${post.metadata.image}`
+              : `${DATA.url}/og?title=${post.metadata.title}`,
+            url: `${DATA.url}/blog/${post.slug}`,
+            author: {
+              "@type": "Person",
+              name: DATA.name,
+            },
+          }),
+        }}
+      />
+      <h1 className="max-w-[650px] font-medium text-2xl tracking-tighter title">
+        {post.metadata.title}
+      </h1>
+      <div className="flex justify-between items-center mt-2 mb-8 max-w-[650px] text-sm">
+        <Suspense fallback={<p className="h-5" />}>
+          <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+            {formatDate(post.metadata.publishedAt)}
+          </p>
+        </Suspense>
+      </div>
+      <article
+        className="dark:prose-invert prose"
+        dangerouslySetInnerHTML={{ __html: post.source }}></article>
+    </section>
+  );
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,45 @@
+import BlurFade from "@/components/magicui/blur-fade";
+import { getBlogPosts } from "@/data/blog";
+import Link from "next/link";
+
+export const metadata = {
+  title: "Blog",
+  description: "My thoughts on software development, life, and more.",
+};
+
+const BLUR_FADE_DELAY = 0.04;
+
+export default async function BlogPage() {
+  const posts = await getBlogPosts();
+
+  return (
+    <section>
+      <BlurFade delay={BLUR_FADE_DELAY}>
+        <h1 className="mb-8 font-medium text-2xl tracking-tighter">blog</h1>
+      </BlurFade>
+      {posts
+        .sort((a, b) => {
+          if (
+            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
+          ) {
+            return -1;
+          }
+          return 1;
+        })
+        .map((post, id) => (
+          <BlurFade delay={BLUR_FADE_DELAY * 2 + id * 0.05} key={post.slug}>
+            <Link
+              className="flex flex-col space-y-1 mb-4"
+              href={`/blog/${post.slug}`}>
+              <div className="flex flex-col w-full">
+                <p className="tracking-tight">{post.metadata.title}</p>
+                <p className="h-6 text-muted-foreground text-xs">
+                  {post.metadata.publishedAt}
+                </p>
+              </div>
+            </Link>
+          </BlurFade>
+        ))}
+    </section>
+  );
+}
