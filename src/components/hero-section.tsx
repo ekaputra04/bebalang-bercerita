0e70c03e2df9caa724af7cea0d0f187ab0046d45
ekaputra04 <ekaputrajuniawan@gmail.com>
2025-06-27 10:08:44 +0800

    feat: blog survey pedana

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -9,16 +9,15 @@ export default function HeroSection() {
   return (
     <section id="hero">
       <div className="space-y-8 mx-auto w-full max-w-3xl">
-        <div className="flex justify-between gap-2">
-          <div className="flex flex-col flex-1 space-y-1.5">
+        <div className="flex justify-between items-center gap-2">
+          <div className="flex flex-col flex-1 space-y-3">
             <BlurFadeText
               delay={BLUR_FADE_DELAY}
               className="bg-clip-text bg-gradient-to-b from-black dark:from-white to-gray-300/80 dark:to-slate-900/10 font-bold text-transparent text-2xl xl:text-5xl/none"
-              yOffset={8}
               text={`${DATA.name}`}
             />
             <BlurFadeText
-              className="max-w-[600px] text-lg md:text-xl"
+              className="text-muted-foreground text-lg md:text-xl"
               delay={BLUR_FADE_DELAY}
               text={DATA.description}
             />
